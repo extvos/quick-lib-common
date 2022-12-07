6acee5288cc4eabfefc333df77baea6a5a23f399
Mingcai SHEN <archsh@gmail.com>
2022-12-07 10:42:42 +0800

    Add logo supporting.

--- src/main/java/plus/extvos/common/utils/QrCode.java
+++ src/main/java/plus/extvos/common/utils/QrCode.java
@@ -10,8 +10,11 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +30,10 @@ public class QrCode {
         private String format = "png";
         private String contents;
 
+        private BufferedImage logo;
+
         private Generator() {
+            this.logo = null;
         }
 
         public Generator size(int s) {
@@ -55,10 +61,21 @@ public class QrCode {
             return this;
         }
 
-        public BufferedImage bufferedImage() throws Exception {
-            Map<EncodeHintType, ErrorCorrectionLevel> hints = new HashMap<>();
-            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        public Generator logo(String filename) throws IOException {
+            this.logo = ImageIO.read(new File(filename));
+            return this;
+        }
 
+        public Generator logo(File file) throws IOException {
+            this.logo = ImageIO.read(file);
+            return this;
+        }
+
+        public BufferedImage bufferedImage() throws Exception {
+            Map<EncodeHintType, Object> hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            hints.put(EncodeHintType.MARGIN, 1);
             QRCodeWriter writer = new QRCodeWriter();
             BitMatrix bitMatrix = null;
             ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -66,23 +83,68 @@ public class QrCode {
             // Create a qr code with the url as content and a size of WxH px
             bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, size, size, hints);
             MatrixToImageConfig matrixConfig = new MatrixToImageConfig(
-                frontColor, bgColor);
+                    frontColor, bgColor);
             // Load QR image
             BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix, matrixConfig);
 
+            if (this.logo != null) {
+                qrImage = combineLogo(qrImage, logo);
+            }
+
             // Initialize combined image
-            BufferedImage combined = new BufferedImage(qrImage.getHeight(), qrImage.getWidth(), BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g = (Graphics2D) combined.getGraphics();
+//            BufferedImage combined = new BufferedImage(qrImage.getHeight(), qrImage.getWidth(), BufferedImage.TYPE_INT_ARGB);
+//            Graphics2D g = (Graphics2D) combined.getGraphics();
 
             // Write QR code to new image at position 0/0
-            g.drawImage(qrImage, 0, 0, null);
-            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-            return combined;
+//            g.drawImage(qrImage, 0, 0, null);
+//            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+            return qrImage;
         }
 
         public boolean write(OutputStream out) throws Exception {
             return ImageIO.write(bufferedImage(), format, out);
         }
+    }
+
+    public static BufferedImage combineLogo(BufferedImage matrixImage, BufferedImage logoImage) {
+        /**
+         * 读取二维码图片，并构建绘图对象
+         */
+        Graphics2D g2 = matrixImage.createGraphics();
+
+        int matrixWidth = matrixImage.getWidth();
+        int matrixHeigh = matrixImage.getHeight();
+        //开始绘制图片
+//        g2.setColor(Color.white);
+//        g2.drawRect(matrixWidth / 5 * 2, matrixHeigh / 5 * 2, matrixWidth / 5, matrixHeigh / 5);
+        g2.drawImage(logoImage, matrixWidth / 5 * 2, matrixHeigh / 5 * 2, matrixWidth / 5, matrixHeigh / 5, Color.WHITE, null);//绘制
+        BasicStroke stroke = new BasicStroke(5, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+        g2.setStroke(stroke);// 设置笔画对象
+        //指定弧度的圆角矩形
+        RoundRectangle2D.Float round = new RoundRectangle2D.Float(matrixWidth / 5 * 2, matrixHeigh / 5 * 2, matrixWidth / 5, matrixHeigh / 5, 20, 20);
+        g2.setColor(Color.white);
+//        g2.fillRoundRect(matrixWidth / 5 * 2, matrixHeigh / 5 * 2, matrixWidth / 5, matrixHeigh / 5, 20, 20);
+//        g2.setColor(Color.black);
+        g2.draw(round);// 绘制圆弧矩形
+
+        //设置logo 有一道灰色边框
+        BasicStroke stroke2 = new BasicStroke(1, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+        g2.setStroke(stroke2);// 设置笔画对象
+        RoundRectangle2D.Float round2 = new RoundRectangle2D.Float(matrixWidth / 5 * 2 + 2, matrixHeigh / 5 * 2 + 2, matrixWidth / 5 - 4, matrixHeigh / 5 - 4, 20, 20);
+        g2.setColor(new Color(128, 128, 128));
+        g2.draw(round2);// 绘制圆弧矩形
+
+        g2.dispose();
+        matrixImage.flush();
+        return matrixImage;
+    }
+
+    public static Generator logo(String f) throws IOException {
+        return new Generator().logo(f);
+    }
+
+    public static Generator logo(File f) throws IOException {
+        return new Generator().logo(f);
     }
 
     public static Generator format(String f) {
