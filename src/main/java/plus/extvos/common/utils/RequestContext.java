a25687231401383b2273e35a2f68abead62f3fe2
Mingcai SHEN <archsh@gmail.com>
2023-09-12 11:53:51 +0800

    Updated.

--- src/main/java/plus/extvos/common/utils/RequestContext.java
+++ src/main/java/plus/extvos/common/utils/RequestContext.java
@@ -21,19 +21,12 @@ public class RequestContext {
 
     private static String LANGUAGE_HEADER = "Accept-Language";
 
-    private static final List<String> POSSIBLE_IP_HEADERS = Arrays.asList(
-            "X-Forwarded-For",
-            "HTTP_FORWARDED",
-            "HTTP_FORWARDED_FOR",
-            "HTTP_X_FORWARDED",
-            "HTTP_X_FORWARDED_FOR",
-            "HTTP_CLIENT_IP",
-            "HTTP_VIA",
-            "HTTP_X_CLUSTER_CLIENT_IP",
-            "Proxy-Client-IP",
-            "WL-Proxy-Client-IP",
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR", "HTTP_X_FORWARDED", "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP", "HTTP_FORWARDED_FOR", "HTTP_FORWARDED", "HTTP_VIA",
             "REMOTE_ADDR"
-    );
+    };
 
     protected RequestContext(HttpServletRequest req) {
         request = req;
@@ -48,17 +41,13 @@ public class RequestContext {
     }
 
     public String getIpAddress() {
-        for (String ipHeader : POSSIBLE_IP_HEADERS) {
-            String headerValue = Collections.list(request.getHeaders(ipHeader)).stream()
-                    .filter(StringUtils::hasLength)
-                    .findFirst()
-                    .orElse(null);
-
-            if (headerValue != null) {
-                return headerValue;
+        for (String ipHeader : IP_HEADER_CANDIDATES) {
+            String ip = request.getHeader(ipHeader);
+            if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+                return ip;
             }
         }
-        return "";
+        return request.getRemoteAddr();
 //        String ip = request.getHeader("x-forwarded-for");
 //        if (ip == null || ip.length() == 0) {
 //            ip = request.getHeader("Proxy-Client-IP");
